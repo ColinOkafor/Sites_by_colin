f5101512a3ec977c50b920d3a2f18b221691876a
Colin Okafor <colinokafor116@gmail.com>
2025-12-15 16:54:13 -0500

    frontend updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Sites_by_colin",
+  base: `/`,
   server: {
     port: 2006, // <-- change this to your desired port
   },
